73d0e371df801d15a8eb810f43a63ed14bcf1b4f
Eduard de Torres <edetorres@salleurl.edu>
2016-06-02 17:12:35 +0200

    Final commit for Connection Activity. Pending: Real connection to Bluetooth device.

--- app/src/main/java/com/lasalle/lsmaker_remote/activities/ConnectionActivity.java
+++ app/src/main/java/com/lasalle/lsmaker_remote/activities/ConnectionActivity.java
@@ -144,16 +144,7 @@ public class ConnectionActivity extends AppCompatActivity {
         // the progress spinner.
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB_MR2) {
             int shortAnimTime = getResources().getInteger(android.R.integer.config_shortAnimTime);
-
-            /*mLoginFormView.setVisibility(show ? View.GONE : View.VISIBLE);
-            mLoginFormView.animate().setDuration(shortAnimTime).alpha(
-                    show ? 0 : 1).setListener(new AnimatorListenerAdapter() {
-                @Override
-                public void onAnimationEnd(Animator animation) {
-                    mLoginFormView.setVisibility(show ? View.GONE : View.VISIBLE);
-                }
-            });*/
-
+            
             mProgressView.setVisibility(show ? View.VISIBLE : View.GONE);
             mProgressView.animate().setDuration(shortAnimTime).alpha(
                     show ? 1 : 0).setListener(new AnimatorListenerAdapter() {
